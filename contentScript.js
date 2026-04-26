b3d72f0a0c03922dbbae00a93cdd348bae3e09c0
Sudhakar-PJ <pjsudhakarpalanisamy@gmail.com>
2026-04-26 19:19:11 +0530

    Enlarge bookmark icon to 34px and add hover effect

--- contentScript.js
+++ contentScript.js
@@ -62,10 +62,14 @@
       bookmarkBtn.style.verticalAlign = "top";
       
       bookmarkBtn.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="#ff0000" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" style="filter: drop-shadow(0 0 2px rgba(0,0,0,0.5));">
+        <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="#ff0000" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" style="filter: drop-shadow(0 0 3px rgba(0,0,0,0.8));">
           <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
         </svg>
       `;
+
+      bookmarkBtn.style.transition = "transform 0.1s ease-in-out";
+      bookmarkBtn.onmouseenter = () => { bookmarkBtn.style.transform = "scale(1.15)"; };
+      bookmarkBtn.onmouseleave = () => { bookmarkBtn.style.transform = "scale(1)"; };
 
       youtubeLeftControls.appendChild(bookmarkBtn);
       bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
